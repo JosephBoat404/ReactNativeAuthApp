9d6de04320bbf0c6754f7c0149d6b5bf3c36622d
Joseph Boateng <josephboatengcollege45@gmail.com>
2025-01-25 12:23:40 -0500

    fixed eslint format

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,25 @@
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import globals from 'globals';
-
 export default [
-  js.configs.recommended,
-  react.configs.recommended,
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    js.configs.recommended,
+    react.configs.recommended,
+    {
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      languageOptions: {
+        parserOptions: {
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+        },
+        globals: {
+          ...globals.browser,
+          ...globals.node,
+        },
       },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
+      plugins: {
+        react, // Now correctly set as an object
+      },
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-console': 'warn',
+        'react/prop-types': 'off',
       },
     },
-    plugins: {
-      react, // This is now correctly set as an object
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'no-console': 'warn',
-      'react/prop-types': 'off',
-    },
-  },
-];
+  ];
